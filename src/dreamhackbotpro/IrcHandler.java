63502b70aa6da470375b2f70d4f8c46925f9bb44
Patrik Weibull <patrik.weibull@gmail.com>
2011-07-19 00:55:28 +0200

    Added .gitignore. Added pircbot and started on IrcHandler, now extending PircBot

--- src/dreamhackbotpro/IrcHandler.java
+++ src/dreamhackbotpro/IrcHandler.java
@@ -6,12 +6,13 @@ package dreamhackbotpro;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.jibble.pircbot.PircBot;
 
 /**
  *
  * @author patrik
  */
-public class IrcHandler implements ConversationsListener {
+public class IrcHandler extends PircBot implements ConversationsListener {
     //TODO extend PircBot
     
     private List<IrcListener> listeners = new ArrayList<IrcListener>();
@@ -24,6 +25,49 @@ public class IrcHandler implements ConversationsListener {
     }
 
     @Override
+    protected void onMessage(String channel, String sender, String login, String hostname, String message) {
+        for(IrcListener l : listeners) {
+            l.onMessage(new Message(sender, message, null));
+        }
+    }
+
+    @Override
+    protected void onPrivateMessage(String sender, String login, String hostname, String message) {
+        for(IrcListener l : listeners) {
+            l.onPrivateMessage(new Message(sender, message, null));
+        }
+    }
+
+    @Override
+    protected void onNickChange(String oldNick, String login, String hostname, String newNick) {
+        for(IrcListener l : listeners) {
+            l.onNameChange(oldNick,newNick);
+        }
+    }
+
+    private void onQuit(String nick) {
+        for(IrcListener l : listeners) {
+            l.onQuit(nick);
+        }
+    }
+
+    @Override
+    protected void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {
+        onQuit(sourceNick);
+    }
+
+    @Override
+    protected void onPart(String channel, String sender, String login, String hostname) {
+        onQuit(sender);
+    }
+
+    @Override
+    protected void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
+        onQuit(recipientNick);
+    }
+
+    @Override
     public void onConversationMessage(Message m) {
+        this.sendMessage(m.to, m.message);
     }
 }
