872568bf8d9f42e485641b5bea60b3a3bd74baa3
Patrik Weibull <patrik.weibull@gmail.com>
2011-07-19 17:30:22 +0200

    Started on Main.java

--- src/dreamhackbotpro/Main.java
+++ src/dreamhackbotpro/Main.java
@@ -10,7 +10,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        IrcHandler irc = new IrcHandler("Monsquaz","irc.quakenet.org","#dreamhack.trade");
+        Conversation.addConversationsListener(irc);
+        Bot bot = new Bot();
+        irc.addIrcListener(bot);
+        GUI gui = new GUI();
+        irc.addIrcListener(gui);
+        Conversation.addConversationsListener(gui);
     }
 
 }
