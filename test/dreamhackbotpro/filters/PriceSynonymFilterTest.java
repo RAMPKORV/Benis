7e80ac5ec97d0bc7ee52adff4b385d36b603029d
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-07-19 23:09:40 +0200

    initiaded PriceSynonymFilter

---
+++ test/dreamhackbotpro/filters/PriceSynonymFilterTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dreamhackbotpro.filters;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author wasd
+ */
+public class PriceSynonymFilterTest extends MessageFilterTest {
+
+    public PriceSynonymFilterTest() {
+        super(new PriceSynonymFilter());
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of filter method, of class PriceSynonymFilter.
+     */
+    @Test
+    public void testFilter() {
+        checkFromTo("säljer snus 100 spänn!", "säljer snus 100kr!");
+        checkFromTo("säljer 24 tum skärm för 2 lax", "säljer 24 tum skärm för 2000kr");
+        checkFromTo("köper cigg 50:-", "köper cigg 50kr");
+        checkFromTo("säljer 3 burkar läsk för en tjuga", "säljer 3 burkar läsk för 20kr");
+        checkFromTo("50 kr", "50kr");
+    }
+
+}
