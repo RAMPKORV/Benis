5c6d0ebacd6fbb4a75167376918125c5060ef317
Patrik Weibull <patrik.weibull@gmail.com>
2011-07-19 22:45:39 +0200

    Added filter to masterfilter

--- src/dreamhackbotpro/filters/MasterFilter.java
+++ src/dreamhackbotpro/filters/MasterFilter.java
@@ -7,6 +7,7 @@ package dreamhackbotpro.filters;
 public class MasterFilter extends CompositeMessageFilter {
     public MasterFilter() {
         addMessageFilter(new WhiteSpaceCleanerFilter());
+        addMessageFilter(new DelimeterFilter());
         addMessageFilter(new TradingSynonymFilter());
     }
 }
