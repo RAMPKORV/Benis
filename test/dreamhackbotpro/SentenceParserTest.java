6d7e12581541da88654612e8167e741c9f56960c
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-07-20 17:17:11 +0200

    added SentenceParserTest

---
+++ test/dreamhackbotpro/SentenceParserTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dreamhackbotpro;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author patrik
+ */
+public class SentenceParserTest {
+
+    private SentenceParser parser = SentenceParser.getInstance();
+
+    public SentenceParserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testParseSimple() {
+        expectedInterest("WTB snus 50kr", "snus", 50, true);
+        expectedInterest("WTS snus 50kr", "snus", 50, false);
+    }
+    
+    private void expectedInterest(String sentence, String thing, int price, boolean wtb){
+        checkFromTo(thing, thing+'-'+price+'-'+wtb);
+    }
+    
+    private void checkFromTo(String from, String to) {
+        assertEquals(to, parser.parseSentence(from).toString());
+    }
+
+}
