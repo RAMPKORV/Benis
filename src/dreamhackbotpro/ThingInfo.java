625167388fcab8be85dcd1270553ca2472452529
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-07-22 01:37:59 +0200

    minPrice and maxPrice finally works as intended in ThingInfo

--- src/dreamhackbotpro/ThingInfo.java
+++ src/dreamhackbotpro/ThingInfo.java
@@ -17,8 +17,8 @@ public class ThingInfo implements Comparable {
     private String thing = null;
     private int counter = 0;
     private List<Integer> prices = new ArrayList<Integer>();
-    private int minPrice = -1;
-    private int maxPrice = -1;
+    private int minPrice = 0;
+    private int maxPrice = 0;
     private int buyers = 0;
     private int sellers = 0;
     private boolean medianCalculated = false;
@@ -86,11 +86,13 @@ public class ThingInfo implements Comparable {
         else
             sellers++;
         
-        //FIXME: some stuff get -1 as minPrice and a normal number such as 50 as maxPrice
-        if(i.getPrice()>maxPrice || maxPrice==-1)
-            maxPrice=i.getPrice();
-        if(i.getPrice()<minPrice || minPrice==-1)
-            minPrice=i.getPrice();
+        if(i.getPrice()>0){
+            if(i.getPrice()>maxPrice)
+                maxPrice=i.getPrice();
+            if(i.getPrice()<minPrice || minPrice==0)
+                minPrice=i.getPrice();
+        }
+        
         medianCalculated = false;
         stdDevCalculated = false;
     }
