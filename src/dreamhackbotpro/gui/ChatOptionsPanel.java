e7bbf8739b5ca8a734872dc5b4b6a7ecdc678b98
Alexander Gunnarsson <sir_alex67@hotmail.com>
2011-07-25 21:45:56 +0200

    added a List Mark in GUI that can be used to keep track of unread messages

--- src/dreamhackbotpro/gui/ChatOptionsPanel.java
+++ src/dreamhackbotpro/gui/ChatOptionsPanel.java
@@ -1,6 +1,9 @@
 package dreamhackbotpro.gui;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
@@ -8,18 +11,32 @@ import javax.swing.JPanel;
  *
  * @author wasd
  */
-public class ChatOptionsPanel extends JPanel {
+public class ChatOptionsPanel extends JPanel implements ActionListener{
 
     private JCheckBox autoscroll;
+    private JButton updateListMark;
+    private GUI gui;
 
-    public ChatOptionsPanel() {
+    public ChatOptionsPanel(GUI gui) {
         super(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        this.gui=gui;
 
         autoscroll = new JCheckBox("Autoscroll");
         add(autoscroll);
+
+        updateListMark = new JButton("Update List Mark");
+        updateListMark.addActionListener(this);
+        add(updateListMark);
     }
     
     public boolean isAutoScroll() {
         return autoscroll.isSelected();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==updateListMark){
+            gui.updateListMark();
+        }
     }
 }
