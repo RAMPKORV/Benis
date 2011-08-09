9ba06f8d73247a2851ed7ecdd1fba66a85b3d003
Patrik Weibull <patrik.weibull@gmail.com>
2011-08-09 18:08:35 +0200

    Fixed regex in Confuse

--- src/dreamhackbotpro/Confuse.java
+++ src/dreamhackbotpro/Confuse.java
@@ -43,7 +43,7 @@ public class Confuse {
     }
 
     public static String negateConfusion(String msg) {
-        msg = msg.replaceAll("(?i)^(ha)*(h | )?(va|what|vafan|wtf|va i helvete|lol)(\\!\\?)*$", getSoundsGood());
+        msg = msg.replaceAll("(?i)^(ha)*(h | )?(va|what|vafan|wtf|va i helvete|lol)[\\!\\?]*$", getSoundsGood());
         msg = msg.replaceAll("(?i)^\\?+$", getSoundsGood());
         return msg;
     }
