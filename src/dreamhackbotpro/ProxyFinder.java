5d56dac4645ec3c8d6e91c8a4b0fb6b550d9247b
Patrik Weibull <patrik.weibull@gmail.com>
2011-11-23 21:10:00 +0100

    Worked more on proxies

--- src/dreamhackbotpro/ProxyFinder.java
+++ src/dreamhackbotpro/ProxyFinder.java
@@ -1,52 +1,82 @@
 package dreamhackbotpro;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
 import java.net.Proxy;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ProxyFinder {
-    
+
     private static ProxyFinder instance = null;
-    
     private List<String> untested = new ArrayList<String>();
     private List<String> notWorking = new ArrayList<String>();
-    private List<Proxy> working = new ArrayList<Proxy>();
+    private Stack<Proxy> working = new Stack<Proxy>();
 
-    private ProxyFinder(){
+    private ProxyFinder() {
         new Thread(new Runnable() {
+
             @Override
             public void run() {
                 try {
-                while(true) {
-                    //TODO: Find new potential proxies to test
-                    for(String proxyToTry: untested) {
-                        //TODO: Test HTTP
-                        //TODO: Test SOCKS4
-                        //TODO: Test SOCKS5
-                        //TODO: Test other proxies
-                        Thread.sleep(1000);
+                    while (true) {
+                        //TODO: Find new potential proxies to test
+                        boolean isWorking = false;
+                        for (String proxyToTry : untested) {
+                            String[] parts = proxyToTry.split(":");
+                            String host = parts[0];
+                            try {
+                                int port = Integer.parseInt(parts[1]);
+                                URL url = new URL(host + ":" + port);
+                                Proxy toTry = null;
+                                Proxy.Type[] proxyTypes = {Proxy.Type.HTTP, Proxy.Type.SOCKS, Proxy.Type.DIRECT};
+                                for (Proxy.Type type : proxyTypes) {
+                                    toTry = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(host, port));
+                                    try {
+                                        url.openConnection(toTry);
+                                        isWorking = true;
+                                        untested.remove(proxyToTry);
+                                        working.add(toTry);
+                                        break;
+                                    } catch (IOException ex) {
+                                    }
+                                }
+                                if(!isWorking) {
+                                    notWorking.add(proxyToTry);
+                                    untested.remove(proxyToTry);
+                                }
+                                Thread.sleep(1000);
+                            } catch (MalformedURLException ex) {
+                            } catch (NumberFormatException ex) {
+                            }
+                        }
+                        if (untested.isEmpty()) {
+                            Thread.sleep(1000);
+                        }
                     }
-                    if(untested.isEmpty()) {
-                        Thread.sleep(1000);
-                    }
-                }
-                } catch(InterruptedException ex) {
+                } catch (InterruptedException ex) {
                     return;
                 }
             }
         }).start();
     }
 
-    public static synchronized ProxyFinder getInstance(){
-        if(instance==null)
+    public static synchronized ProxyFinder getInstance() {
+        if (instance == null) {
             instance = new ProxyFinder();
+        }
         return instance;
     }
 
-    public Proxy getWorkingProxy(){
-        return new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("202.70.52.66", 1080));
+    public Proxy getWorkingProxy() {
+        if (working.isEmpty()) {
+            return null;
+        }
+        return working.pop();
     }
-
-
 }
