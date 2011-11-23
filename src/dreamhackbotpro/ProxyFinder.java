29f45ca26c10b3241c2e22b3ab2d9a898990d5b2
Patrik Weibull <patrik.weibull@gmail.com>
2011-11-23 18:38:08 +0100

    Started working on proxies.

--- src/dreamhackbotpro/ProxyFinder.java
+++ src/dreamhackbotpro/ProxyFinder.java
@@ -1,11 +1,41 @@
 package dreamhackbotpro;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProxyFinder {
     
     private static ProxyFinder instance = null;
+    
+    private List<String> untested = new ArrayList<String>();
+    private List<String> notWorking = new ArrayList<String>();
+    private List<Proxy> working = new ArrayList<Proxy>();
 
     private ProxyFinder(){
-        //TODO: Create proxy checking thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                while(true) {
+                    //TODO: Find new potential proxies to test
+                    for(String proxyToTry: untested) {
+                        //TODO: Test HTTP
+                        //TODO: Test SOCKS4
+                        //TODO: Test SOCKS5
+                        //TODO: Test other proxies
+                        Thread.sleep(1000);
+                    }
+                    if(untested.isEmpty()) {
+                        Thread.sleep(1000);
+                    }
+                }
+                } catch(InterruptedException ex) {
+                    return;
+                }
+            }
+        }).start();
     }
 
     public static synchronized ProxyFinder getInstance(){
@@ -14,8 +44,8 @@ public class ProxyFinder {
         return instance;
     }
 
-    public String getWorkingProxy(){
-        return null;
+    public Proxy getWorkingProxy(){
+        return new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("202.70.52.66", 1080));
     }
 
 
